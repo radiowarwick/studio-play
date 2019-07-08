313a98f771d1d9c3d44ada455e723bc0898b78de
Connor Powell <c.powell924@gmail.com>
2019-07-08 22:17:12 +0100

    added some comments

--- src/components/audiowallWall.jsx
+++ src/components/audiowallWall.jsx
@@ -7,20 +7,30 @@ import Grid from '@material-ui/core/Grid';
 class AudiowallWall extends Component {
     render() {
         const { items } = this.props.wall;
+        
+        // Item array to store our audiowall items for rendering
         let itemArray = [];
-
+        // Seen keeps track of our current index (in items)
         let seen = 0;
+        // array runs through all 12 items of a wall
+        // This ensures that when there is a gap between items
+        // it is appropriately filled with an empty item element
         for(let i = 0; i < 12; i++) {
+            // Load an item, this may be null if seen is out of range
             const item = items[seen];
+
+            // If item is not null, and the items location matches our i, add it to render it
             if(item && item.item === i) {
-                seen++
+                seen++;
                 itemArray.push(<AudiowallItem item={item} key={i} />);
             }
+            // If our i doesn't match our current seen index, add a null item
             else {
                 itemArray.push(<AudiowallItem item={null} key={i} />);
             }
         }
 
+        // Return a grid container of our items 
         return (
             <Grid container spacing={1}>{itemArray}</Grid>
         );
